e0bd6bc46406e78e6e23925f87415c0a1e28a1de
Anthony Castaneda <lcastaa.aql@gmail.com>
2024-12-17 19:20:15 -0600

    sorry

--- src/main/java/net/tutorial/basic_app/controller/WebPageController.java
+++ src/main/java/net/tutorial/basic_app/controller/WebPageController.java
@@ -1,4 +1,16 @@
 package net.tutorial.basic_app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class WebPageController {
+
+
+    @GetMapping("/greeting")
+    public String sayHi(){
+        return "hi.html";
+    }
+
 }
